7f13d36d082fe979eda7a5cbdca62b0b10b54da3
JapaProgramadora <76882368+JapaProgramadora@users.noreply.github.com>
2023-06-21 19:47:59 -0300

    1st commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const productBaseUrl =
+      'https://store-db-7ba62-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://store-db-7ba62-default-rtdb.firebaseio.com/orders';
+}
