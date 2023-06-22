d42c6bc10d21a46cff0309229afa79e1c1941a85
JapaProgramadora <76882368+JapaProgramadora@users.noreply.github.com>
2023-06-22 12:38:47 -0300

    auth configuration

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
@@ -21,8 +23,34 @@ class AuthPage extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 10,
+                  ),
+                  transform: Matrix4.rotationZ((-8 * pi )/ 1800)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26,
+                        offset: Offset(0,2)
+                      )
+                    ]
+                  ),
+                  child: const Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
               ],
             ),
           ),
